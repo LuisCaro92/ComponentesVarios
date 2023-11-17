529e0e0ad158c4169d3160dad559f2ba0a22a12b
luis caro <luis.carosalgado1@gmail.com>
2023-11-17 14:27:09 -0300

    inputs creados

---
+++ src/componentes/Dropdown.js
@@ -0,0 +1,13 @@
+import Select from "react-select";
+
+
+const Dropdown =()=>{
+
+    return(
+        <div>
+            <Select className="border-radius-sm"/>
+        </div>
+    )
+}
+
+export default Dropdown;
--- src/componentes/Send.js
+++ src/componentes/Send.js
@@ -1,17 +1,18 @@
 import { Fragment } from "react";
+import Dropdown from "./Dropdown";
 
 const Send = () => {
   return (
     <Fragment>
-      <div className="bg-gray-800  justify-center min-h-screen py-20">
+      <div className="bg-gray-800  justify-center min-h-screen py-10">
         <div className="mx-auto  flex container-center ">
           <div className="flex w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
             
-            <div className="w-full lg:w-1/2 py-16 px-12">
+            <div className="w-full lg:w-1/2 py-8 px-12">
               <h2 className="text-3xl mb-2">Enviar correo</h2>
-              <p className="mb-4"> Favor ingresa los datos </p>
+              
               <form action="#">
-                <div className=" mt-5">
+                <div className=" mt-3">
                     <h1 className="text-start font-bold">Para</h1>
                   <input
                     type="text"
@@ -19,31 +20,36 @@ const Send = () => {
                     className="border border-gray-200 py-1 px-2 rounded-lg w-full shadow-sm hover:"
                   />
                 </div>
-                <div className=" mt-5">
+                <div className=" mt-3">
+                <h1 className="text-start font-bold">Correo</h1>
                   <input
-                    type="password"
-                    placeholder="Paswword"
+                    type="text"
+                    placeholder="Ingrese correo"
                     className="border border-gray-200 py-1 px-2 rounded-lg w-full shadow-sm hover:"
                   />
                 </div>
-                <div className=" mt-5">
+                <div className=" mt-3">
+                <h1 className="text-start font-bold">Asunto</h1>
                   <input
-                    type="password"
-                    placeholder="Paswword"
+                    type="text"
+                    placeholder="Ingrese asunto"
                     className="border border-gray-200 py-1 px-2 rounded-lg w-full shadow-sm hover:"
                   />
                 </div>
-                <div className=" mt-5">
+                <div className=" mt-3">
+                    <h1 className="text-start font-bold">Previsualizar</h1>
+                <Dropdown/>
+                </div>
+                <div className=" mt-3">
+                <h1 className="text-start font-bold">Mensaje</h1>
                   <input
-                    type="password"
-                    placeholder="Paswword"
-                    className="border border-gray-200 py-1 px-2 rounded-lg w-full shadow-sm hover:"
+                    type="text"
+                    className="border border-gray-200 py-1 px-2 rounded-lg w-full h-20 shadow-sm hover:"
                   />
                 </div>
+               
 
-                <div className="mt-5"></div>
-
-                <div className="mt-10">
+                <div className="mt-5">
                   <button className="active:scale-[.98] actiove:duration-75 transition-all hover:scale-[1.02] ease-in-out w-full  bg-[#60B478] rounded-xl h-8 border border-solid border-gray-400 text-center text-white font-bold shadow-lg">
                     <span className="shadow-sm">Enviar</span>
                   </button>
@@ -54,8 +60,7 @@ const Send = () => {
               <h1 className="text-white text-3xl">Welcome</h1>
               <div>
                 <p className="mt-5 mb-20 pb-40 text-dark">
-                  Bienvenido a App-Todo-List. Inicia sesion para comenzar a
-                  organizar tu lista de tareas.
+                 Acá va el previsuaizador
                 </p>
               </div>
             </div>
