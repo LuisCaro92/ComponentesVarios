0c5a2ff2dc90b1cbf2169123c4c84e33716bd532
luis caro <luis.carosalgado1@gmail.com>
2023-11-17 23:09:09 -0300

    tarea terminada

--- src/componentes/Send.js
+++ src/componentes/Send.js
@@ -7,33 +7,10 @@ const previOption = [
 ];
 
 const Send = () => {
-  const InputField = ({
-    label,
-    placeholder,
-    type = "text",
-    onChange,
-    value,
-  }) => (
-    <div className="mt-3">
-      <h1 className="text-start font-bold">{label}</h1>
-      <input
-        type={type}
-        placeholder={placeholder}
-        className="border border-gray-200 py-2 px-2 rounded-sm w-full shadow-sm hover:"
-        onChange={onChange}
-        value={value}
-      />
-    </div>
-  );
-
   const [email, setEmail] = useState("");
   const [inputValue, setInputValue] = useState("");
   const [asunto, setAsunto] = useState("");
   const [mensaje, setMensaje] = useState("");
-
-  const handleSelectChange = (selectedOption) => {
-    console.log(selectedOption);
-  };
 
   return (
     <Fragment>
@@ -43,30 +20,41 @@ const Send = () => {
             <div className="w-full lg:w-1/2 py-8 px-12">
               <h2 className="text-3xl mb-2">Enviar correo</h2>
               <form action="#">
-                <InputField
-                  label="Para"
-                  placeholder="Seleccione base de datos"
-                  onChange={(e) => setInputValue(e.target.value)}
-                  value={inputValue}
-                />
-                <InputField
-                  label="Correo"
-                  placeholder="example@mail.com"
-                  onChange={(e) => setEmail(e.target.value)}
-                  value={email}
-                />
-                <InputField
-                  label="Asunto"
-                  placeholder="Ingrese asunto"
-                  onChange={(e) => setAsunto(e.target.value)}
-                  value={asunto}
-                />
+                <div className="mt-3">
+                  <h1 className="text-start font-bold">Para</h1>
+
+                  <input
+                    className="border border-gray-200 py-2 px-2 rounded-sm w-full shadow-sm hover:"
+                    placeholder="Seleccione base de datos"
+                    onChange={(e) => setInputValue(e.target.value)}
+                    value={inputValue}
+                  />
+                </div>
+
+                <div className="mt-3">
+                  <h1 className="text-start font-bold">Asunto</h1>
+                  <input
+                    className="border border-gray-200 py-2 px-2 rounded-sm w-full shadow-sm hover:"
+                    placeholder="Ingrese asunto"
+                    onChange={(e) => setAsunto(e.target.value)}
+                    value={asunto}
+                  />
+                </div>
+                <div className="mt-3">
+                  <h1 className="text-start font-bold">Correo</h1>
+                  <input
+                    className="border border-gray-200 py-2 px-2 rounded-sm w-full shadow-sm hover:"
+                    placeholder="Example@mail.com"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                  />
+                </div>
                 <div className="mt-3">
                   <h1 className="text-start font-bold">Previsualizar</h1>
                   <Select
                     className="border-radius-sm"
                     options={previOption}
-                    onChange={handleSelectChange}
+                    
                   />
                 </div>
                 <div className="mt-3 ">
@@ -83,12 +71,12 @@ const Send = () => {
                 </div>
               </form>
             </div>
-            <div className="w-full lg:w-1/2 flex flex-col mt-40">
-              <div className="w-80 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            <div className="w-full lg:w-1/2  mt-40">
+            <div className="divide-y divide-gray-500 dark:divide-gray-700 w-80 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <h5 className=" mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
                   {asunto}
                 </h5>
-                <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+                <p className=" mt-3 mb-10 font-normal text-gray-500 dark:text-gray-400">
                   {mensaje}
                 </p>
               </div>
