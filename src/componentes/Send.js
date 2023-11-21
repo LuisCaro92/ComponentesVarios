0e69465d6db4ab0dc465e9a87512d442323b8781
luis caro <luis.carosalgado1@gmail.com>
2023-11-21 17:57:10 -0300

    cambios finales, todo ok, solo falta que la previsualizacion sea responsive y estaria terminado

--- src/componentes/Send.js
+++ src/componentes/Send.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from "react";
 import Select from "react-select";
+import Swal from "sweetalert2";
 
+
+//Mensaje de previsualizado transformado en una constante.
 const Previsualizacion = ({ asunto, mensaje, enviarCorreo }) => (
-  <div className="divide-y-5 divide-gray-500 dark:divide-gray-700 w-80 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <div className="none divide-y-5 divide-gray-500 dark:divide-gray-700 w-80 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ms:ml-60">
     <h5 className="first-letter:mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
       {asunto}
     </h5>
@@ -22,6 +25,7 @@ const Previsualizacion = ({ asunto, mensaje, enviarCorreo }) => (
 );
 
 const Send = () => {
+    //Estados.
   const [email, setEmail] = useState("");
   const [asunto, setAsunto] = useState("");
   const [mensaje, setMensaje] = useState("");
@@ -29,6 +33,7 @@ const Send = () => {
   const [selectedBodegas, setSelectedBodegas] = useState([]);
   const [error, setError] = useState("");
 
+  //Base de datos JSON de prueba.
   const bodegas = [
     { id: 1, nombre: "Almacén Central" },
     { id: 2, nombre: "Depósito Principal" },
@@ -38,34 +43,55 @@ const Send = () => {
     { id: 6, nombre: "Almacén de Herramientas" },
     { id: 7, nombre: "Deposito de ropas" },
   ];
-
+ 
+  //Funcion para leer base de datos.
   const bodegasOptions = bodegas.map((bodega) => ({
     value: bodega.id,
     label: bodega.nombre,
   }));
 
-  const handleBodegasChange = (selectedOptions) => {
-    setSelectedBodegas(selectedOptions);
-  };
-
+ 
+  //Seleccionar opcion de previsualizar mensaje.
   const handleSelectChange = (selectedOption) => {
     setMostrarPrevisual(selectedOption && selectedOption.value === true);
   };
 
+  //Seleccionar más de un destinatario en base de datos
+  const handleBodegasChange = (selectedOptions) => {
+    setSelectedBodegas(selectedOptions);
+  };
+  const validateEmail = (email) => {
+    // Validar el formato del correo electrónico
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+
   const enviarCorreo = () => {
     // Validar campos antes de enviar
-    if (!email || !asunto || !mensaje || selectedBodegas.length === 0) {
-      setError("Por favor, complete todos los campos.");
-      return;
-    }
+    if (!email) {
+      setError("Por favor, ingrese su correo electrónico.");
+    } else if (!validateEmail(email)) {
+      setError("Por favor, ingrese un correo electrónico válido.");
+    } else if (!asunto) {
+      setError("Por favor, complete el campo 'Asunto'.");
+    } else if (!mensaje) {
+      setError("Por favor, complete el campo 'Mensaje'.");
+    } else if (selectedBodegas.length === 0) {
+      setError("Por favor, seleccione al menos una opción en 'Para'.");
+    } else {
+      // Lógica para enviar el correo
+      Swal.fire({
+        title: "Mensaje enviado con exito!",
+        text: "Haz clic en el botón!",
+        icon: "success"
+      });
 
-    // Lógica para enviar el correo
-    console.log("Correo enviado");
-
-    // Reiniciar el formulario después de enviar
-    window.alert("Correo enviado con éxito");
-    window.location.reload();
-  
+      //Reiniciar formulario
+      setTimeout(() => {
+        window.location.reload();
+      }, 4000);
+    };
   };
 
   return (
@@ -76,42 +102,38 @@ const Send = () => {
             <div className="w-full lg:w-1/2 py-8 px-12">
               <h2 className="text-3xl mb-2">Enviar correo</h2>
               <form action="#">
-                <div className={`mt-3 ${error && "border-red-500"}`}>
+                <div className={`mt-3 ${!selectedBodegas.length && error ? "border-red-500":""}`}>
                   <h1 className="text-start font-bold">Para</h1>
                   <Select
-                    className={`border-radius-sm ${error && "border-red-500"}`}
+                    className={`border-radius-sm ${!selectedBodegas.length && error ? "border-red-500":""}`}
                     options={bodegasOptions}
                     isMulti
                     onChange={handleBodegasChange}
                     value={selectedBodegas}
                   />
                 </div>
-                <div className={`mt-3 ${error && "border-red-500"}`}>
+                <div className={`mt-3 ${!asunto && error ? 'border-red-500' : ''}`}>
                   <h1 className="text-start font-bold">Asunto</h1>
                   <input
-                    className={`border border-gray-200 py-2 px-2 rounded-sm w-full shadow-sm hover: ${
-                      error && "border-red-500"
-                    }`}
+                    className={`border border-gray-200 py-2 px-2 rounded-sm w-full shadow-sm hover: ${!asunto && error ? 'border-red-500' : ''}`}
                     placeholder="Ingrese asunto"
                     onChange={(e) => setAsunto(e.target.value)}
                     value={asunto}
                   />
                 </div>
-                <div className={`mt-3 ${error && "border-red-500"}`}>
+                <div className={`mt-3 ${(!email || !validateEmail(email)) && error ? 'border-red-500' : ''}`}>
                   <h1 className="text-start font-bold">Correo</h1>
                   <input
-                    className={`border border-gray-200 py-2 px-2 rounded-sm w-full shadow-sm hover: ${
-                      error && "border-red-500"
-                    }`}
+                    className={`border border-gray-200 py-2 px-2 rounded-sm w-full shadow-sm hover: ${(!email || !validateEmail(email)) && error ? 'border-red-500' : ''}`}
                     placeholder="Example@mail.com"
                     onChange={(e) => setEmail(e.target.value)}
                     value={email}
                   />
                 </div>
-                <div className={`mt-3 ${error && "border-red-500"}`}>
+                <div className={`mt-3 ${!mostrarPrevisual && error ? 'border-red-500' : ''}`}>
                   <h1 className="text-start font-bold">Previsualizar</h1>
                   <Select
-                    className={`border-radius-sm ${error && "border-red-500"}`}
+                    className={`border-radius-sm ${!mostrarPrevisual && error ? 'border-red-500' : ''}`}
                     options={[
                       { label: "Si", value: true },
                       { label: "No", value: false },
@@ -119,16 +141,19 @@ const Send = () => {
                     onChange={handleSelectChange}
                   />
                 </div>
-                <div className={`mt-3 ${error && "border-red-500"}`}>
+                <div className={`mt-3 ${!mensaje && error ? 'border-red-500' : ''}`}>
                   <h1 className="text-start font-bold">Mensaje</h1>
                   <textarea
-                    className={`h-20 border border-gray-200 py-2 px-2 rounded-sm w-full shadow-sm ${
-                      error && "border-red-500"
-                    }`}
+                    className={`h-20 border border-gray-200 py-2 px-2 rounded-sm w-full shadow-sm ${!mensaje && error ? 'border-red-500' : ''}`}
                     onChange={(e) => setMensaje(e.target.value)}
                   />
                 </div>
-                {error && <p className="text-red-500 mt-2">{error}</p>}
+                
+              {error && (
+                <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-red-800 dark:text-red-400" role="alert">
+                  <span className="font-medium">{error}</span>
+                </div>
+              )}
                 <div className="mt-5">
                   <button
                     className="active:scale-[.98] actiove:duration-75 transition-all hover:scale-[1.02] ease-in-out w-full bg-[#60B478] rounded-lg h-10 border border-solid border-gray-400 text-center text-white font-bold shadow-lg"
